e25267970f3f1b5a0a37290da80b24d2eea05037
Sakie Kawamichi <sakie0313@gmail.com>
2022-03-04 06:40:10 +0000

    [Add]VC:new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,30 @@
 class BooksController < ApplicationController
   def new
+    @new_book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+  if @book.save
+      redirect_to book_path(@book.id), flash[:notice] = "Book was successfully created."
+    else 
+     render :index
+    end
   end
 
   def index
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
