19349d1830511187c7959547c217530caf9e7b21
Sakie Kawamichi <sakie0313@gmail.com>
2022-03-04 08:13:05 +0000

    [Add]VC:edit,update,destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,18 @@
 class BooksController < ApplicationController
-  def new
-    @new_book = Book.new
-  end
-
+  
   def create
-    @book = Book.new(book_params)
-    if @book.save
+    @new_book = Book.new(book_params)
+    if @new_book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@new_book.id)
     else
       render :index
     end
   end
 
   def index
+    @books = Book.all
+    @new_book = Book.new
   end
 
   def show
@@ -21,6 +20,20 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
