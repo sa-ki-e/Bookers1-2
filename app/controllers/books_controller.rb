7e7769d3612366b71561821096cc0f23afa5df07
Sakie Kawamichi <sakie0313@gmail.com>
2022-03-04 06:44:53 +0000

    [Add]C:create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,14 @@ class BooksController < ApplicationController
   def new
     @new_book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
-  if @book.save
-      redirect_to book_path(@book.id), flash[:notice] = "Book was successfully created."
-    else 
-     render :index
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
     end
   end
 
@@ -21,9 +22,9 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
